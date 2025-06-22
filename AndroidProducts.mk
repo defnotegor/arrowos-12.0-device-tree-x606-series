e49a393c6c395bca1bb8958098df22a6e208f315
Egor Ikonnikov <seelenchik4@gmail.com>
2025-06-22 09:08:25 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,14 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The LineageOS Project  
+# Copyright (C) 2025 ArrowOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_achilles6_row_wifi.mk
+    $(LOCAL_DIR)/arrow_achilles6.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_achilles6_row_wifi-user \
-    lineage_achilles6_row_wifi-userdebug \
-    lineage_achilles6_row_wifi-eng
+    arrow_achilles6-user \
+    arrow_achilles6-userdebug \
+    arrow_achilles6-eng
