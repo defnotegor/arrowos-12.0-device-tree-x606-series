a3e4e82d66b18f6d22ed71d6214e50507cf15427
Egor Ikonnikov <seelenchik4@gmail.com>
2025-06-22 09:07:57 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,59 +1,186 @@
 #
 # Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2025 ArrowOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+DEVICE_PATH := device/lenovo/achilles6
 
-# Overlays
-PRODUCT_ENFORCE_RRO_TARGETS := *
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/lenovo/achilles6/achilles6-vendor.mk)
 
-# Product characteristics
-PRODUCT_CHARACTERISTICS := tablet
-
-# Rootdir
-PRODUCT_PACKAGES += \
-
-PRODUCT_PACKAGES += \
-    fstab.enableswap \
-    init.connectivity.rc \
-    meta_init.rc \
-    factory_init.connectivity.rc \
-    init.modem.rc \
-    init.ago.rc \
-    factory_init.rc \
-    init.aee.rc \
-    multi_init.rc \
-    meta_init.modem.rc \
-    init.mt8768.rc \
-    factory_init.project.rc \
-    init.mt6765.usb.rc \
-    init.project.rc \
-    init.sensor_1_0.rc \
-    meta_init.project.rc \
-    meta_init.connectivity.rc \
-    init.mt6765.rc \
-    ueventd.rc \
-    init.recovery.mt8768.rc \
-    init.recovery.mt6765.rc \
-    init.rc \
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.service \
+    android.hardware.soundtrigger@2.3-impl \
+    audio.bluetooth.default \
+    audio.primary.mt8768 \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudiopreprocessing \
+    libbundlewrapper \
+    libdownmix \
+    libdynproc \
+    libeffectproxy \
+    libldnhncr \
+    libreverbwrapper \
+    libvisualizer
+
+# Audio configuration files
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(DEVICE_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(DEVICE_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1-service.btlinux \
+    libbluetooth_audio_session
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.6-impl \
+    android.hardware.camera.provider@2.6-service \
+    libcamera2ndk_vendor
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.allocator@4.0-service.mediatek \
+    android.hardware.graphics.mapper@4.0-service.mediatek \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    libdrm
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4-service.widevine
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-mediatek \
+    android.hardware.gnss@2.1-service
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1-service.mediatek
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.lenovo_mt8768
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(DEVICE_PATH)/configs/media/media_codecs_mediatek_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_mediatek_video.xml \
+    $(DEVICE_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(DEVICE_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.faketouch.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.faketouch.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
+
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3-service.mediatek
+
+# Properties
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.6-service.mediatek \
+    android.hardware.radio.config@1.3-service.mediatek
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    fstab.mt8768 \
+    init.mt8768.rc \
+    init.mt8768.usb.rc \
+    init.recovery.mt8768.rc
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.1-service.mediatek \
+    libsensorndkbridge
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(DEVICE_PATH)
 
-# Audio policy configuration
-USE_XML_AUDIO_POLICY_CONF := 1
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.mediatek
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.dual_role_usb
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.3-impl \
+    android.hardware.vibrator@1.3-service
 
-# Inherit the proprietary files
-# $(call inherit-product, vendor/lenovo/achilles6_row_wifi/achilles6_row_wifi-vendor.mk)
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.5-service.mediatek \
+    hostapd \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# Wi-Fi firmware symlinks
+PRODUCT_PACKAGES += \
+    wifi_symlinks
